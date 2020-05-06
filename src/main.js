e78cecbac8986ec03195eddc4fad644c4b539152
xzb <1102059895@qq.com>
2020-05-06 18:20:37 +0800

    基础模板

--- src/main.js
+++ src/main.js
@@ -3,8 +3,28 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import FastClick from 'fastclick'
+// 瀑布流
+import waterfall from 'vue-waterfall2'
+// 全局引入UI库 vant
+import '@/plugins/vant'
+// 全局引入rem
+import '@/config/rem'
+// 全局引入 Reset-css
+import 'reset-css'
+//  全局引入 过滤器
+import '@/config/filter'
 
 Vue.config.productionTip = false
+
+// 解决移动端点击延迟200ms的问题
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function () {
+      FastClick.attach(document.body);
+  }, false);
+}
+
+Vue.use(waterfall)
 
 new Vue({
   router,
