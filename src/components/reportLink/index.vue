bd5780f72d146b0b052801438092d77fadb55ddb
xzb <1102059895@qq.com>
2020-06-05 18:12:54 +0800

    基础报备关联

---
+++ src/components/reportLink/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="reportLink">
+    <van-field v-model="searchForm.Q" center clearable label="关联项目">
+      <template #input>
+        <div>
+          测试数据
+          <van-tag type="primary">+3</van-tag>
+        </div>
+      </template>
+    </van-field>
+  </div>
+</template>
+<script>
+export default {
+  name: "reportLink",
+  props: {
+    buildList: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    openDetail(item) {}
+  }
+};
+</script>
+<style lang="less" scoped>
+.reportLink {
+}
+</style>
